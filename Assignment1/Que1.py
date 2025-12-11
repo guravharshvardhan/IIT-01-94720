aba316ad7022de037e94d69cfd3e6ebbf741b114
Harshvardhan Gurav <harshvardhangurav2005@gmail.com>
2025-12-11 22:18:07 +0530

    assignment1

--- Assignment1/Que1.py
+++ Assignment1/Que1.py
@@ -1,1 +1,21 @@
+sentence = input("Enter a sentence: ")
+
+# Number of characters
+num_chars = len(sentence)
+
+# Number of words
+words = sentence.split()
+num_words = len(words)
+
+# Number of vowels
+vowels = "aeiouAEIOU"
+num_vowels = 0
+
+for ch in sentence:
+    if ch in vowels:
+        num_vowels += 1
+
+print("Characters:", num_chars)
+print("Words:", num_words)
+print("Vowels:", num_vowels)
 
