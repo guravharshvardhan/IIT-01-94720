fa82cb36b086fe91a36ffad4cece635f7fe0165c
Harshvardhan Gurav <harshvardhangurav2005@gmail.com>
2025-12-13 12:00:41 +0530

    submit1

--- Assignment2/Que1/main.py
+++ Assignment2/Que1/main.py
@@ -1,1 +1,19 @@
+import math_utils
 
+# Circle
+r = float(input("Enter radius of circle: "))
+print("\n Area of circle =", math_utils.area_circle(r))
+
+# Rectangle
+l = float(input("Enter rectangle length: "))
+w = float(input("Enter rectangle width: "))
+print("\n Area of rectangle =", math_utils.area_rectangle(l, w))
+
+# Triangle
+b = float(input("Enter base of triangle: "))
+h = float(input("Enter height of triangle: "))
+print("\n Area of triangle =", math_utils.area_triangle(b, h))
+
+#square
+s=float(input("enter side of square"))
+print("\n Area of square=",math_utils.area_square(h))
---
+++ Assignment2/Que1/math_utils.py
@@ -0,0 +1,13 @@
+import math
+
+def area_circle (r:float):
+    return math.pi*r*r
+
+def area_rectangle(l:float,w:float):
+    return l*w
+
+def area_triangle(b:float,h:float):
+    return 0.5*b*h
+
+def area_square(s:float):
+    return s*s
