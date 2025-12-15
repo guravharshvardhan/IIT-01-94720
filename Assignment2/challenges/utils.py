36947c5d261dc8e0964acb8b8a3ae6f088554a13
Harshvardhan Gurav <harshvardhangurav2005@gmail.com>
2025-12-15 11:32:18 +0530

    submit

--- Assignment2/challenges/utils.py
+++ Assignment2/challenges/utils.py
@@ -1,1 +1,16 @@
 
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def get_weather(city):
+    api_key = os.getenv("OPENWEATHER_API_KEY")
+
+    url = (
+        "https://api.openweathermap.org/data/2.5/weather"f"?q={city}&appid={api_key}&units=metric"
+    )
+
+    response = requests.get(url, timeout=10)
+    return response.json()
--- Assignment2/challenges/weather.py
+++ Assignment2/challenges/weather.py
@@ -1,1 +1,19 @@
+from dotenv import load_dotenv
+import os
+from utils import get_weather
 
+load_dotenv()  # loads .env file
+
+def get_Whea():
+    data = get_weather(city)
+    # print(f"Full Data : \n {data}")
+    temp = data["main"]["temp"]
+    humidity = data["main"]["humidity"]
+
+    print("\n--- Weather Report ---")
+    print(f"Temperature: {temp} °C")
+    print(f"Humidity: {humidity} %")
+
+if __name__ == "__main__":
+    city = input("Enter city name: ")
+    get_Whea()
