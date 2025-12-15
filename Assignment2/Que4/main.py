2564bc019f5a62a432cb4ce1cbb2b1d0acc51c3e
Harshvardhan Gurav <harshvardhangurav2005@gmail.com>
2025-12-15 08:48:23 +0530

    submit

--- Assignment2/Que4/main.py
+++ Assignment2/Que4/main.py
@@ -1,1 +1,15 @@
+from dotenv import load_dotenv
+import os
+import requests
+load_dotenv()
 
+api_key = os.getenv("API_KEY")
+city = input("Enetr the City name : ")
+
+url=f"https://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city}&units=metric"
+
+response = requests.get(url)
+
+weather = response.json()
+
+print(weather)
